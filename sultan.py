5a2245265cf76bd05bd3c90be672b3a9b68e5da6
samof76 <samof76@yahoo.com>
2018-05-26 16:00:07 +0530

    Make it slackaware

--- sultan.py
+++ sultan.py
@@ -2,90 +2,39 @@ from errbot import BotPlugin, botcmd, arg_botcmd
 from sultan.api import Sultan
 from itertools import chain
 
-CONFIG_TEMPLATE = {'username': 'root'}
-
-def configure(self, configuration):
-    if configuration is not None and configuration != {}:
-        config = dict(chain(CONFIG_TEMPLATE.items(),
-                            configuration.items()))
-    else:
-        config = CONFIG_TEMPLATE
-    super(Newrelic, self).configure(config)
+username = 'root'
 
 class SultanBot(BotPlugin):
-
-  def get_configuration_template(self):
-    return CONFIG_TEMPLATE
 
   @arg_botcmd('hostname', type=str)
   def sultan_get_uptime(self, msg, hostname=None):
     """
     Get the uptime from the host
     """
-    username = self.config['username']
     if not hostname:
       return 'Sultan will only respond, if you provide a hostname'
     
     with Sultan.load(hostname=hostname,user=username) as s:
-      rtn = s.sudo('uptime').run()
+      uptime = s.sudo('uptime')
     
-    return "/code {}".format("\n".join(rtn.stdout))
+    rtn = uptime.run()
+    
+    return "``` {}\n```".format("\n".join(rtn.stdout))
 
   @arg_botcmd('hostname', type=str)
   def sultan_tail_dmesg(self, msg, hostname=None):
     """
     Get the tail dmesg from the host
     """
-    username = self.config['username']
     if not hostname:
       return 'Sultan will only respond, if you provide a hostname'
     
     with Sultan.load(hostname=hostname,user=username) as s:
-      rtn = s.sudo('dmesg | tail').run()
+      tail_dmesg = s.sudo('dmesg | tail')
     
-    return "/code {}".format("\n".join(rtn.stdout))
+    rtn = tail_dmesg.run()
+    return "``` {}\n```".format("\n".join(rtn.stdout))
 
-  @arg_botcmd('hostname', type=str)
-  def sultan_get_monit_status(self, msg, hostname=None):
-    """
-    Get the monit status from the host
-    """
-    username = self.config['username']
-    if not hostname:
-      return 'Sultan will only respond, if you provide a hostname'
-    
-    with Sultan.load(hostname=hostname,user=username) as s:
-      rtn = s.sudo('monit status').run()
-    
-    return "/code {}".format("\n".join(rtn.stdout))
-
-  @arg_botcmd('hostname', type=str)
-  def sultan_get_monit_summary(self, msg, hostname=None):
-    """
-    Get the monit summary from the host
-    """
-    username = self.config['username']
-    if not hostname:
-      return 'Sultan will only respond, if you provide a hostname'
-    
-    with Sultan.load(hostname=hostname,user=username) as s:
-      rtn = s.sudo('monit summary').run()
-    
-    return "/code {}".format("\n".join(rtn.stdout))
-
-  @arg_botcmd('hostname', type=str)
-  def sultan_get_passenger_status(self, msg, hostname=None):
-    """
-    Get the passenger status from the host
-    """
-    username = self.config['username']
-    if not hostname:
-      return 'Sultan will only respond, if you provide a hostname'
-    
-    with Sultan.load(hostname=hostname,user=username) as s:
-      rtn = s.sudo('passenger-status').run()
-    
-    return "/code {}".format("\n".join(rtn.stdout))
 
 
   @arg_botcmd('hostname', type=str)
@@ -93,14 +42,15 @@ class SultanBot(BotPlugin):
     """
     Get the mount points from the host
     """
-    username = self.config['username']
     if not hostname:
       return 'Sultan will only respond, if you provide a hostname'
     
     with Sultan.load(hostname=hostname,user=username) as s:
-      rtn = s.sudo('mount').run()
+      mnt_points = s.sudo('mount')
     
-    return "/code {}".format("\n".join(rtn.stdout))
+    rtn = mnt_points.run()
+    
+    return "``` {}\n".format("\n".join(rtn.stdout))
 
 
   @arg_botcmd('hostname', type=str)
@@ -108,14 +58,15 @@ class SultanBot(BotPlugin):
     """
     Get the disk usage from the host
     """
-    username = self.config['username']
     if not hostname:
       return 'Sultan will only respond, if you provide a hostname'
     
     with Sultan.load(hostname=hostname,user=username) as s:
-      rtn = s.sudo('df -h').run()
+      disk_usage = s.sudo('df -h')
     
-    return "/code {}".format("\n".join(rtn.stdout))
+    rtn = disk_usage.run()
+    
+    return "``` {}\n```".format("\n".join(rtn.stdout))
 
   @arg_botcmd('hostname', type=str)
   def sultan_get_memory_usage(self, msg, hostname=None):
@@ -127,35 +78,39 @@ class SultanBot(BotPlugin):
       return 'Sultan will only respond, if you provide a hostname'
     
     with Sultan.load(hostname=hostname,user=username) as s:
-      rtn = s.sudo('free -m').run()
+      mem_free = s.sudo('free -m')
     
-    return "/code {}".format("\n".join(rtn.stdout))
+    rtn = mem_free.run()
+    
+    return "``` {}\n```".format("\n".join(rtn.stdout))
 
   @arg_botcmd('hostname', type=str)
   def sultan_get_top_memory_hoggers(self, msg, hostname=None):
     """
     Get the top memory hoggers from the host
     """
-    username = self.config['username']
     if not hostname:
       return 'Sultan will only respond, if you provide a hostname'
     
     with Sultan.load(hostname=hostname,user=username) as s:
-      rtn = s.sudo('ps -eo pid,ppid,cmd,%mem,%cpu --sort -rss | head').run()
+      top_mem_hoggers = s.sudo('ps -eo pid,ppid,cmd,%mem,%cpu --sort -rss | head')
     
-    return "/code {}".format("\n".join(rtn.stdout))
+    rtn = top_mem_hoggers.run()
+    
+    return " ``` {}\n``` ".format("\n".join(rtn.stdout))
 
   @arg_botcmd('hostname', type=str)
   def sultan_get_top_cpu_hoggers(self, msg, hostname=None):
     """
     Get the top CPU hoggers from the host
     """
-    username = self.config['username']
     if not hostname:
       return 'Sultan will only respond, if you provide a hostname'
     
     with Sultan.load(hostname=hostname,user=username) as s:
-      rtn = s.sudo('ps -eo pid,ppid,cmd,%mem,%cpu --sort -%cpu | head').run()
+      top_cpu_hoggers = s.sudo('ps -eo pid,ppid,cmd,%mem,%cpu --sort -%cpu | head')
     
-    return "/code {}".format("\n".join(rtn.stdout))
+    rtn = top_cpu_hoggers.run()
+    
+    return "``` {}\n```".format("\n".join(rtn.stdout))
 
